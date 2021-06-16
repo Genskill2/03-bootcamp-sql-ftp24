bb671fc5422fd978a0e8187d95cbbc99c90c9382
Jesvin Sebastian Madona <sebastianjesvin@gmail.com>
2021-06-16 16:34:00 +0530

    modified create.sql and added insert.sql

--- create.sql
+++ create.sql
@@ -1,6 +1,9 @@
-CREATE TABLE publisher (id INTEGER PRIMARY KEY, name TEXT,country TEXT);
-CREATE TABLE books(id INTEGER PRIMARY KEY, title TEXT,
+CREATE TABLE publisher (id PRIMARY KEY, name TEXT,country TEXT);
+
+CREATE TABLE books(id PRIMARY KEY, title TEXT,
   FOREIGN KEY (publisher) REFERENCES publisher(id));
+
 CREATE TABLE subjects (id INTEGER PRIMARY KEY, name TEXT,country TEXT);
+
 CREATE TABLE books_subjects(FOREIGN KEY(book) REFERENCES books(id),
   FOREIGN KEY (subject) REFERENCES subjects(id));
