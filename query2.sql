b88d362ac498922235438e968a59b3ec1501f36c
Jesvin Sebastian Madona <sebastianjesvin@gmail.com>
2021-06-19 23:18:01 +0530

    added queries 1-3

---
+++ query2.sql
@@ -0,0 +1,1 @@
+SELECT a.name, b.name FROM books a,publisher b WHERE b.country="UK"; 
---
+++ query3.sql
@@ -0,0 +1,2 @@
+SELECT a.name FROM books a, books_subjects b WHERE a.id = b.book AND (b.subject = 3 OR
+b.subject=8);
